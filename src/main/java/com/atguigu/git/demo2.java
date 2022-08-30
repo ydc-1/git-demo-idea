acea1bc4a0fb427ed6c3c0705adb7ef46f05dec2
ydc <1521817536@qq.com>
2022-08-31 00:25:49 +0800

    dev-A分支:第一提交

--- src/main/java/com/atguigu/git/demo2.java
+++ src/main/java/com/atguigu/git/demo2.java
@@ -4,6 +4,8 @@ public class demo2 {
 
     public static void main(String[] args) {
         System.out.println("33333333333333");
+        System.out.println("4444444444444");
+        System.out.println("55555555555555");
     }
 
 }
