a0121607f7cbc5ea823fc0ccf2404808adc5543d
ydc <1521817536@qq.com>
2023-03-15 20:14:43 +0800

    Revert "Revert "dev:第五四次提交到测试仓库中""
    This reverts commit e8b5a5b34c1ac5584c802040b6cc4371c1db9494.

--- src/main/java/com/atguigu/git/demo2.java
+++ src/main/java/com/atguigu/git/demo2.java
@@ -9,6 +9,7 @@ public class demo2 {
         System.out.println("6666666666666677");
         System.out.println("6666666666666688");
         System.out.println("777777777777777777");
+        System.out.println("888888888");
     }
 
 }
